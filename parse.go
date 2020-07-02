721a34cc75d76266c99ef1d8576a6a83b9fcec47
Ivan Sharovarov <sharovarov31@gmail.com>
2020-07-02 20:34:07 +0600

    DFSing the html

--- parse.go
+++ parse.go
@@ -1,6 +1,10 @@
 package html_link_parser
 
-import "io"
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"io"
+)
 
 type Link struct {
 	Href string
@@ -8,5 +12,21 @@ type Link struct {
 }
 
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	dfs(doc, "")
 	return nil, nil
+}
+
+func dfs(n *html.Node, p string) {
+	msg := n.Data
+	if n.Type == html.ElementNode {
+		msg = "<" + msg + ">"
+	}
+	fmt.Println(p, msg)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, p+" ")
+	}
 }
